1d9c3852fa172c569cfb519f5173a4ae6c679aca
Dopin Protocol <dopinfinance@gmail.com>
2024-09-20 05:48:15 +0000

    dopin

--- src/protocols/defi.ts
+++ src/protocols/defi.ts
@@ -1,6 +1,14 @@
+import { Category } from '../constants';
 import { Protocol } from './index';
 
 const defi: Protocol[] = [
+    {
+        name: 'Dopin Protocol',
+        url: 'https://linktr.ee/dopinprotocol',
+        logoPath: '/logos/Logo_Only.svg',
+        category: Category.Defi,
+        twitterUrl: 'https://x.com/Dopinprotocol',
+      }
 ];
 
 export { defi };
